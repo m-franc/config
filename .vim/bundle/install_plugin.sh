e55b8b9bcf0603b866ec88d79fa6e0fcd46810ec
Maxime FRANC <mfranc@e3r2p21.42.fr>
2017-12-12 17:10:26 +0100

    Add script for plug in vim

---
+++ .vim/bundle/install_plugin.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree;
+git clone --depth=1 https://github.com/rust-lang/rust.vim.git ~/.vim/bundle/rust.vim;
+git clone https://github.com/vim-airline/vim-airline;
+git clone https://github.com/vim-airline/vim-airline-themes
