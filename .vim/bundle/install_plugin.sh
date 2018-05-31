40a3ebaae8bebf87c5ad1d2e68923b4db539a5cb
Maxime FRANC <mfranc@vente-privee.com>
2018-05-31 16:20:59 +0200

    Add syntastic cmd

--- .vim/bundle/install_plugin.sh
+++ .vim/bundle/install_plugin.sh
@@ -2,3 +2,5 @@ git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree;
 git clone --depth=1 https://github.com/rust-lang/rust.vim.git ~/.vim/bundle/rust.vim;
 git clone https://github.com/vim-airline/vim-airline;
 git clone https://github.com/vim-airline/vim-airline-themes
+cd ~/.vim/bundle && \
+	git clone --depth=1 https:/`/github.com/vim-syntastic/syntastic.git
